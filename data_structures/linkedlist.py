d83edee62145d1607861794100fd6cc2e26b11b2
CLEMENT-ANDI EDET <cedet@udallas.edu>
2023-08-16 22:38:05 -0500

    created remove and remove_all

--- data_structures/linkedlist.py
+++ data_structures/linkedlist.py
@@ -21,17 +21,44 @@ class ListNode(object):
 
         return self
 
+    def remove(self, entry):
+        node = ListNode(0)
+        node.next = self
+
+        while node.next is not None:
+            if node.next.val == entry:
+                node.next = node.next.next
+            else:
+                node = node.next
+
+    def remove_all(self, entry):
+        rv = ListNode(0)
+        rv.next = self
+        node = rv
+        print(rv)
+        while node.next is not None:
+            if node.next.val == entry:
+                node.next = node.next.next
+            else:
+                node = node.next
+
     def __str__(self):
         return f"{self.val} -> {self.next}"
 
 
-l1 = ListNode(1, ListNode(2, ListNode(4)))
-l2 = ListNode(1, ListNode(3, ListNode(4)))
-l3 = ListNode(0).addLast(1)
-print(l3)
-l3.addLast(4)
-print(l3)
-l3.addFirst(-1)
-print(l3)
-print(l1)
-print(l2)
+if __name__ == '__main__':
+    l1 = ListNode(1, ListNode(2, ListNode(4)))
+    l2 = ListNode(1, ListNode(3, ListNode(4)))
+    l3 = ListNode(0).addLast(1)
+    # print(l3)
+    l3.addLast(4)
+    # print(l3)
+    l3.addFirst(-1)
+    # print(l3)
+    l1.addLast(2)
+    l1.addLast(2)
+    print(l1)
+    l1.remove(2)
+    # l1.remove_all(2)
+    print(l1)
+    # print(l2)
