b5bc1a16054754a931508f831bc0214483029602
CLEMENT-ANDI EDET <cedet@udallas.edu>
2023-08-20 22:31:27 -0500

    overrided the len method, then optimised it by changing surrounding methods

--- data_structures/linkedlist.py
+++ data_structures/linkedlist.py
@@ -2,6 +2,17 @@ class ListNode(object):
     def __init__(self, val, next=None):
         self.val = val
         self.next = next
+        self.size = self._find_current_size()
+
+    def _find_current_size(self):
+        node = self
+        size = 1
+
+        while node.next is not None:
+            node = node.next
+            size += 1
+
+        return size
 
     def addLast(self, other):
         node = ListNode(0)
@@ -13,13 +24,12 @@ class ListNode(object):
                 node.next = ListNode(other)
                 break
 
-        return self
+        self.size += 1
 
     def addFirst(self, other):
         self.next = ListNode(self.val, self.next)
         self.val = other
-
-        return self
+        self.size += 1
 
     def remove(self, entry):
         node = ListNode(0)
@@ -28,6 +38,8 @@ class ListNode(object):
         while node.next is not None:
             if node.next.val == entry:
                 node.next = node.next.next
+                self.size -= 1
+                break
             else:
                 node = node.next
 
@@ -38,6 +50,7 @@ class ListNode(object):
         while node.next is not None:
             if node.next.val == entry:
                 node.next = node.next.next
+                self.size -= 1
             else:
                 node = node.next
 
@@ -53,6 +66,9 @@ class ListNode(object):
 
         return False
 
+    def __len__(self):
+        return self.size
+
     def __str__(self):
         return f"{self.val} -> {self.next}"
 
@@ -60,17 +76,25 @@ class ListNode(object):
 if __name__ == '__main__':
     l1 = ListNode(1, ListNode(2, ListNode(4)))
     l2 = ListNode(1, ListNode(3, ListNode(4)))
-    l3 = ListNode(0).addLast(1)
-    # print(l3)
-    l3.addLast(4)
-    # print(l3)
-    l3.addFirst(-1)
-    # print(l3)
+    # l3 = ListNode(0).addLast(1)
     l1.addLast(2)
-    l1.addLast(2)
-    print(l1)
-    # l1.remove(2)
-    print(l1.__contains__(100))
-    # l1.remove_all(2)
-    print(l1)
+    # print(len(l1))
+    l2.addLast(9)
+    l2.addFirst(8)
     # print(l2)
+    # print(len(l2))
+    # print(l3)
+    # l3.addLast(4)
+    # # print(l3)
+    # l3.addFirst(-1)
+    # # print(l3)
+    # l1.addLast(2)
+    # l1.addLast(2)
+    # print(l1)
+    # # l1.remove(2)
+    # print(l1.__contains__(100))
+    # # l1.remove_all(2)
+    # print(l1)
+    # print(l2)
+    l4 = ListNode(0)
+    print(len(l4))
