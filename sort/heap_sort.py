74b91a4a207f2a553aebe8ef4c22e6b1ff0ccca9
CLEMENT-ANDI EDET <cedet@udallas.edu>
2023-10-16 15:36:12 -0500

    added comments to heap_sort.py

--- sort/heap_sort.py
+++ sort/heap_sort.py
@@ -1,9 +1,11 @@
 from heapq import heapify, heappop
+# from data_structures.heap import MyHeap
 
 if __name__ == '__main__':
     in_ = [20, 5, 7, 9, 2, 1, 23, 1000, -1, -2, 11, 32, 2]
 
     # to sort in ascending order
+    # heapify creates a minimum heap by default
     heapify(in_)
 
     rv = []
@@ -14,6 +16,8 @@ if __name__ == '__main__':
     print(rv)
 
     # to sort in descending order
+    # just flip the signs, this would make the biggest number the smallest
+    # then at popping items, we change flip the sign again
     in_ = [20, 5, 7, 9, 2, 1, 23, 1000, -1, -2, 11, 32, 2]
     in_ = [-1 * num for num in in_]
     heapify(in_)
